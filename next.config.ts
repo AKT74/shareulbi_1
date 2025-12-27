4cd9705029369ea3a6ad0c76d3083450c7ff64e9
AKT74 <nayakat74@gmail.com>
2025-12-27 16:04:54 +0700

    update next.config.ts to include output, images, basePath, and assetPrefix configurations

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+
+  output: "export",
+
+  images: {
+    unoptimized: true,
+  },
+
+  basePath: "/shareulbi_1",
+  assetPrefix: "/shareulbi_1/",
 };
 
 export default nextConfig;
